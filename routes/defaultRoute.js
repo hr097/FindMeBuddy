cb9b90e7214dea5b0141ea68d14a3630c80987be
Harshil Ramani <harshilramani9777@gmail.com>
2023-07-29 18:44:25 +0530

    socket connection test 2

--- routes/defaultRoute.js
+++ routes/defaultRoute.js
@@ -1,4 +1,4 @@
-const {get_socket_id} = require("./index.js");
+const {get_socket_id} = require("../index.js");
 const express = require("express")
 const router = express.Router();
 
@@ -12,7 +12,7 @@ const {defaultRouteRun} = require("../controllers/defaultController.js")
 
 //* DEFAULT ROUTE
 
-const s_id = get_socket_id();
+const s_id = get_socket_id;
 
 //* FUNCTIONS
 const results = {
